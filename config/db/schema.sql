e6a1956c8e76036e8a8a9572b9c623f9a12441fb
GainorB <gainorbostwick@gmail.com>
2017-08-26 01:04:42 -0400

    updates

--- config/db/schema.sql
+++ config/db/schema.sql
@@ -1,15 +1,16 @@
-DROP DATABASE IF EXISTS mvc_app;
-CREATE DATABASE mvc_app;
+DROP DATABASE IF EXISTS express_starter_app;
+CREATE DATABASE express_starter_app;
 
-\c mvc_app;
+\c express_starter_app;
 
-CREATE TABLE IF NOT EXISTS articles(
+CREATE TABLE IF NOT EXISTS users(
     id BIGSERIAL PRIMARY KEY NOT NULL,
-    title TEXT NOT NULL,
-    content TEXT NOT NULL
+    username TEXT,
+    name TEXT -- holds name from FB, Twitter or Google token
+    password TEXT NOT NULL,
+    email TEXT NOT NULL,
+    FaceBookToken TEXT, -- holds facebook token
+    TwitterToken TEXT, -- holds twitter token
+    GoogleToken TEXT, -- holds google token
+    jwtToken TEXT -- holds jwt token
 );
-
--- INSERT into articles(title, content) VALUES('Article 1', 'This is article 1');
--- INSERT into articles(title, content) VALUES('Article 2', 'This is article 2');
--- INSERT into articles(title, content) VALUES('Article 3', 'This is article 3');
--- INSERT into articles(title, content) VALUES('Article 4', 'This is article 4');
--- server.js
+++ server.js
@@ -1,43 +1,108 @@
+// =============================================================
+// IMPORTS
+
 require('dotenv').config(); // SUPPORT .ENV FILES
-const express = require('express'); // BRING IN EXPRESS
+const express = require('express');
 const app = express(); // INITILIZE APP
 const path = require('path');
-const bodyParser = require('body-parser'); 
+const bodyParser = require('body-parser');
 
-const http = require('http'); // CORE MODULE, USED TO CREATE THE HTTP SERVER
+const http = require('http'); // USED TO CREATE THE HTTP SERVER
 const server = http.createServer(app); // CREATE HTTP SERVER USING APP
 const port = process.env.PORT || '3000'; // INITIALIZE DEFAULT PORT OR PORT FROM ENVIRONMENT VARIABLE
 
-const logger = require('morgan'); // TERMINAL LOGGER: SHOWS THE ROUTE AND STATUS CODE FOR EVERY REQUEST
+const logger = require('morgan'); // TERMINAL LOGGER
+const session = require('express-session'); // HANDLE SESSIONS
+const passport = require('passport'); // HANDLE AUTH
+const flash = require('connect-flash'); // FLASH MESSAGES
+const mongoose = require('mongoose');
 
+// =============================================================
+// ESTABLISH CONNECTION WITH MONGO
+
+// mongoose.Promise = global.Promise;
+
+// const db = mongoose.connection;
+
+// if (process.env.NODE_ENV !== 'test') {
+//   mongoose.connect('mongodb://localhost/emotrr', {
+//     useMongoClient: true
+//   });
+
+//   db.on('error', err => console.warn('Warning', err));
+//   db.once('open', () => console.log('Connected to MongoDB!'));
+// }
+
+// =============================================================
+// ROUTES
+
+const auth = require('./routes/authRoutes');
+
+// =============================================================
 // VIEW ENGINE SETUP
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev')); // USE MORGAN
-app.use(bodyParser.urlencoded({ extended: false })); // PARSE application/x-www-form-urlencoded
-app.use(bodyParser.json()); // PARSE application/json
+// =============================================================
+// EXPRESS FLASH MIDDLEWARE
 
-// USE STATIC FILES (CSS, JS, IMAGES)
-app.use(express.static(path.join(__dirname, 'public')));
-
-// CORS
-app.all('/*', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+app.use(require('connect-flash')());
+app.use(function(req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
 });
 
+// =============================================================
+// POSTGRESQL SESSION STORE
+
+app.use(
+  session({
+    store: new (require('connect-pg-simple')(session))(),
+    secret: process.env.SECRET_KEY,
+    resave: true, // changed from false
+    saveUninitialized: true,
+    cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 } // 7 days
+  })
+);
+
+// =============================================================
+// MIDDLEWARE
+
+app.use(logger('dev')); // USE MORGAN
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport')(passport);
+
+// =============================================================
+// CORS
+
+app.all('/*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  next();
+});
+
+// =============================================================
 // SECURITY
+
 app.disable('x-powered-by');
 
-// CONTROLLERS
-app.use('/', require('./controllers/articlesController')); // ARTICLES CONTROLLER
+// =============================================================
+// USE ROUTES
 
-/*
-* START SERVER
-*/
+auth(app);
+
+// =============================================================
+// START SERVER
 
 // SET THE PORT
 app.set('port', port);
@@ -48,11 +113,15 @@ server.listen(port);
 // LOG WHICH PORT THE SERVER IS RUNNING ON
 console.log('Server listening on port ' + port);
 
+// =============================================================
 // ERROR HANDLER
+
 app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(err.status || 500).send(err.stack);
+  console.log(err);
+  res.status(err.status || 500).send(err.stack);
 });
 
+// =============================================================
 // EXPORT APP
+
 module.exports = app;
